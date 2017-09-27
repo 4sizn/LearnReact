b2a115287663832796f5b941dd2467a501f2297e
4sizn <4sizn@naver.com>
2017-09-27 23:56:04 +0900

    Delete, Edit function

--- React-fundamentals/src/components/ContactDetails.js
+++ React-fundamentals/src/components/ContactDetails.js
@@ -1,38 +1,116 @@
-import React from 'react';
+import React from "react";
 
 export default class ContactDetails extends React.Component {
-    render() {
+  constructor(props) {
+    super(props);
 
-        // 선택되었을 때 보여질 부분
-        const details = (
-            <div>
-                <p>{ this.props.contact.name }</p>
-                <p>{ this.props.contact.phone }</p>
-            </div>
-        );
+    this.state = {
+      isEdit: false,
+      name: "",
+      phone: ""
+    };
 
-        // 아무것도 선택되지 않았을 때 보여질 부분
-        const blank = (
-            <div> Nothing is Selected </div>
-        );
+    this.handleToggle = this.handleToggle.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleEdit = this.handleEdit.bind(this);
+  }
 
-        return(
-            <div>
-                <h2>Details</h2>
+  handleToggle() {
+      if(!this.state.isEdit){
+          this.setState({
+              name : this.props.contact.name,
+              phone : this.props.contact.phone
+          });
+      }else{
+          this.handleEdit();
+      }
+    this.setState({
+      isEdit: !this.state.isEdit
+    });
+  }
+  handleChange(e) {
+    let nextState = {};
+    nextState[e.target.name] = e.target.value;
+    this.setState(nextState)
+}
 
-                { /* isSelected props 값에 따라 어떤걸 보여줄지 정한다
-                    ternary expression condition ? true : false */ }
+handleEdit(){
+    this.props.onEdit(this.state.name, this.state.phone);
+}
 
-                { this.props.isSelected ? details : blank }
 
-             </div>
-        );
-    }
+  render() {
+    // 선택되었을 때 보여질 부분
+    const details = (
+      <div>
+        <p>{this.props.contact.name}</p>
+        <p>{this.props.contact.phone}</p>
+      </div>
+    );
+
+    
+    // 아무것도 선택되지 않았을 때 보여질 부분
+    const blank = <div> Nothing is Selected </div>;
+
+    const edit = (
+      <div>
+          <p>
+        <input
+          type="text"
+          name="name"
+          placeholder="name"
+          value={this.state.name}
+          onChange={this.handleChange}
+        />
+        </p>
+        <p>
+        <input
+          type="text"
+          name="phone"
+          placeholder="phone"
+          value={this.state.phone}
+          onChange={this.handleChange}
+        />
+        </p>
+      </div>
+    );
+
+    const view = this.state.isEdit ? edit : details;
+
+    return (
+      <div>
+        <h2>Details</h2>
+
+        {/* isSelected props 값에 따라 어떤걸 보여줄지 정한다
+                    ternary expression condition ? true : false */}
+
+        {this.props.isSelected ? view : blank}
+        <p>
+          <button onClick={this.handleToggle}>
+            {this.state.isEdit ? "OK" : "Edit"}
+          </button>
+          <button onClick={this.props.onRemove}>Remove</button>
+        </p>
+      </div>
+    );
+  }
 }
 
 ContactDetails.defaultProps = {
-    contact: {
-        name: "",
-        phone: ""
-    }
+  contact: {
+    name: "",
+    phone: ""
+  },
+  onRemove: () => {
+    console.error("onRemove not defined");
+  },
+  onEdit: () => {
+    console.error("onEdit not defined");
+  }
+};
+
+ContactDetails.propTypes = {
+    contact : React.propTypes.object,
+    onRemove : React.propTypes.func,
+    onEdit : React.propTypes.func
 }
