b965931e76ac681f6964d03607cc2ba1a38d839c
4sizn <4sizn@naver.com>
2017-10-05 09:09:20 +0900

    fe-react-route-v3

--- React_MeMoApp/server/main.js
+++ React_MeMoApp/server/main.js
@@ -1,23 +1,24 @@
 import express from 'express';
 import path from 'path';
-import WebpackDevServer  from 'webpack-dev-server';
+
 import webpack from 'webpack';
+import WebpackDevServer from 'webpack-dev-server';
 
 const app = express();
 const port = 3000;
 const devPort = 4000;
 
-app.use('/', express.static(path.json(__dirname, './../pubic')));
+app.use('/', express.static(path.join(__dirname, './../public')));
 
-app.get('/hello', (req,res) =>{
-        return res.send('Hello CodeLab');
+app.get('/hello', (req, res) => {
+    return res.send('Hello CodeLab');
 });
 
 app.listen(port, () => {
- consle.log('Express is listening on port', port);
+    console.log('Express is listening on port', port);
 });
 
-if(process.env.NODE_ENV = 'development'){
+if(process.env.NODE_ENV == 'development') {
     console.log('Server is running on development mode');
     const config = require('../webpack.dev.config');
     const compiler = webpack(config);
