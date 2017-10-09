babd7e7e337151872cc07d759283fc2518c96547
4sizn <4sizn@naver.com>
2017-10-09 18:34:07 +0900

    update redux multi counter

--- React-Redux/example-counter/src/actions/index.js
+++ React-Redux/example-counter/src/actions/index.js
@@ -1,20 +1,36 @@
 import * as types from './ActionTypes';
 
-export const increment = () => {
+export const increment = (index) => {
     return {
-    type : types.INCREMENT
+    type : types.INCREMENT,
+    index
     }
 }
 
-export const decrement = () =>{
+export const decrement = (index) =>{
     return {
-    type : types.DECREMENT
+    type : types.DECREMENT,
+    index
     }
 }
 
-export const setColor = (color) => {
+export const setColor = ({color,index}) => {
     return {
     type : types.SET_COLOR,
-    color
+    color,
+    index
+    }
+}
+
+export const create = (color) =>{
+    return {
+        type: types.CREATE,
+        color
+    }
+}
+
+export const remove =() =>{
+    return {
+        type : types.REMOVE
     }
 }
--- React-Redux/example-counter/src/containers/App.js
+++ React-Redux/example-counter/src/containers/App.js
@@ -1,12 +1,28 @@
-import React, { Component } from 'react';
-import CounterContainer from '../containers/CounterContainer';
+import React, {Component} from 'react';
+import Buttons from '../components/Buttons';
+import CounterListContainer from './CounterListContainer';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-export default class App extends Component {
+import { getRandomColor } from '../utils';
+
+class App extends Component {
   render() {
+    const {onCreate, onRemove} = this.props;
+
     return (
-      <div>
-        <CounterContainer></CounterContainer>
+      <div className="App">
+        <Buttons onCreate = {onCreate} onRemove = {onRemove}></Buttons>
+        <CounterListContainer></CounterListContainer>
       </div>
     )
   }
 }
+
+//액션 함수 준비
+const mapToDispatch = (dispatch) =>({
+  onCreate : ()=> dispatch(actions.create(getRandomColor())),
+  onRemove : (index)=> dispatch(actions.remove(index))
+});
+
+export default connect(null, mapToDispatch)(App);
